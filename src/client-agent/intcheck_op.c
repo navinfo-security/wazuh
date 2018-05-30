33542f94f38541237b03521890ebe13eefe5c288
Borja Arroba <borja.arroba@wazuh.com>
2018-05-30 12:52:42 +0200

    Updates sending alerts on wazuh configuration files

--- src/client-agent/intcheck_op.c
+++ src/client-agent/intcheck_op.c
@@ -11,18 +11,26 @@
 #include "agentd.h"
 #include "os_crypto/md5/md5_op.h"
 #include "os_crypto/sha1/sha1_op.h"
+#include "os_crypto/sha256/sha256_op.h"
+#include "os_crypto/md5_sha1/md5_sha1_op.h"
+#include "os_crypto/md5_sha1_sha256/md5_sha1_sha256_op.h"
 
 
 /* Send integrity checking information about a file to the server */
 int intcheck_file(const char *file_name, const char *dir)
 {
     struct stat statbuf;
-    os_md5 mf_sum = "";
-    os_sha1 sf_sum = "";
-    char newsum[912 + 1];
+    os_md5 mf_sum;
+    os_sha1 sf_sum;
+    os_sha256 sf256_sum;
+    char newsum[PATH_MAX+4];
+
+    strncpy(mf_sum,  "xxx", 4);
+    strncpy(sf_sum,  "xxx", 4);
+    strncpy(sf256_sum, "xxx", 4);
 
     newsum[0] = '\0';
-    newsum[912] = '\0';
+    newsum[1172] = '\0';
 
     /* Stat the file */
 #ifdef WIN32
@@ -31,7 +39,7 @@ int intcheck_file(const char *file_name, const char *dir)
     if (lstat(file_name, &statbuf) < 0)
 #endif
     {
-        snprintf(newsum, 911, "%c:%s:-1 %s%s", SYSCHECK_MQ, SYSCHECK,
+        snprintf(newsum, sizeof(newsum), "%c:%s:-1 %s%s", SYSCHECK_MQ, SYSCHECK,
                  dir, file_name);
         send_msg(newsum, -1);
 
@@ -45,25 +53,27 @@ int intcheck_file(const char *file_name, const char *dir)
     if (S_ISREG(statbuf.st_mode) || S_ISLNK(statbuf.st_mode))
 #endif
     {
-        /* Generate SHA-1 of the file */
-        if (OS_SHA1_File(file_name, sf_sum, OS_TEXT) < 0) {
-            strncpy(sf_sum, "xxx", 4);
-        }
-
-        /* Generate MD5 of the file */
-        if (OS_MD5_File(file_name, mf_sum, OS_TEXT) < 0) {
-            strncpy(mf_sum, "xxx", 4);
+        if (OS_MD5_SHA1_SHA256_File(file_name, NULL, mf_sum, sf_sum, sf256_sum, OS_BINARY) < 0)
+        {
+            strncpy(mf_sum, "n/a", 4);
+            strncpy(sf_sum, "n/a", 4);
+            strncpy(sf256_sum, "n/a", 4);
         }
     }
+    
 
-    snprintf(newsum, 911, "%c:%s:%d:%d:%d:%d:%s:%s %s%s",
-             SYSCHECK_MQ, SYSCHECK,
-             (int)statbuf.st_size,
-             (int)statbuf.st_mode,
-             (int)statbuf.st_uid,
-             (int)statbuf.st_gid,
-             mf_sum,
-             sf_sum, dir, file_name);
+    snprintf(newsum, sizeof(newsum), "%c:%s:%d:%d:%d:%d:%s:%s:%s %s%s",
+            SYSCHECK_MQ,
+            SYSCHECK,
+            (int)statbuf.st_size,
+            (int)statbuf.st_mode,
+            (int)statbuf.st_uid,
+            (int)statbuf.st_gid,
+            mf_sum,
+            sf_sum,
+            sf256_sum,
+            dir, 
+            file_name);
 
     send_msg(newsum, -1);
     return (1);
