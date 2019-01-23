cc0ab77c0e0cb4d00a569ed9e0775c8870a453d9
Vikman Fernandez-Castro <victor@wazuh.com>
2019-01-23 20:39:43 +0100

    Fix file copying at WPK bootstrap for Windows

--- src/win32/upgrade.bat
+++ src/win32/upgrade.bat
@@ -2,9 +2,10 @@
 
 IF "%1"=="B" GOTO background
 
-COPY -Y upgrade\upgrade.bat . > NUL
-COPY -Y upgrade\do_upgrade.ps1 . > NUL
-COPY -Y upgrade\wazuh-agent-*.msi . > NUL
+COPY /Y upgrade\upgrade.bat . > NUL
+COPY /Y upgrade\do_upgrade.ps1 . > NUL
+COPY /Y upgrade\wazuh-agent-*.msi . > NUL
+COPY /Y upgrade\do_upgrade.bat . > NUL
 
 START /B upgrade.bat B
 GOTO end
@@ -14,11 +15,11 @@ SLEEP 5 2> NUL || ping -n 5 127.0.0.1 > NUL
 powershell -ExecutionPolicy ByPass -File do_upgrade.ps1
 
 IF "%ERRORLEVEL%"=="9009" (
-    COPY -Y upgrade\do_upgrade.bat . > NUL
     do_upgrade.bat
-    DEL do_upgrade.bat
+
 )
 
+DEL do_upgrade.bat
 DEL do_upgrade.ps1
 DEL wazuh-agent-*.msi
 DEL upgrade.bat
