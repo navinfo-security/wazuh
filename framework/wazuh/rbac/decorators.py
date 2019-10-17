9099e58e652f6679621ddc65c196510341cb4180
Daniel Ruiz <daniel.ruiz@wazuh.com>
2019-10-17 13:50:49 +0200

    Refactor merging errors logic

--- framework/wazuh/rbac/decorators.py
+++ framework/wazuh/rbac/decorators.py
@@ -10,7 +10,7 @@ from wazuh.common import rbac, system_agents
 from wazuh.core.core_utils import get_agents_info, expand_group, get_groups
 from wazuh.exception import WazuhError, create_exception_dic
 from wazuh.rbac.orm import RolesManager, PoliciesManager
-from wazuh.results import WazuhResult
+from wazuh.results import WazuhResult, AffectedItemsWazuhResult
 
 #mode = configuration.read_api_config()['rbac']['mode']
 mode = 'white'
@@ -186,14 +186,14 @@ def _get_required_permissions(actions: list = None, resources: list = None, **kw
     """
     # We expose required resources for the request
     res_list = list()
-    target_params = list()
+    target_params = dict()
     add_denied = True
     for resource in resources:
-        m = re.search(r'^([a-z*]+:[a-z*]+:)(\w+|\*|{(\w+)})$', resource)
+        m = re.search(r'^([a-z*]+:[a-z*]+):(\w+|\*|{(\w+)})$', resource)
         res_base = m.group(1)
         # If we find a '{' in the regex we obtain the dynamic resource/s
         if '{' in m.group(2):
-            target_params.append(m.group(3))
+            target_params[m.group(1)] = m.group(3)
             if m.group(3) in kwargs:
                 # Dynamic resources ids are found within the {}
                 params = kwargs[m.group(3)]
@@ -202,20 +202,20 @@ def _get_required_permissions(actions: list = None, resources: list = None, **kw
                     if len(params) == 0:
                         raise WazuhError(4015, extra_message={'param': m.group(3)})
                     for param in params:
-                        res_list.append("{0}{1}".format(res_base, param))
+                        res_list.append("{0}:{1}".format(res_base, param))
                 else:
                     if params is None or params == '*':
                         add_denied = True
                         params = '*'
-                    res_list.append("{0}{1}".format(res_base, params))
+                    res_list.append("{0}:{1}".format(res_base, params))
             # KeyError occurs if required dynamic resources can't be found within request parameters
             else:
                 add_denied = False
                 params = '*'
-                res_list.append("{0}{1}".format(res_base, params))
+                res_list.append("{0}:{1}".format(res_base, params))
         # If we don't find a regex match we obtain the static resource/s
         else:
-            target_params.append(m.group(2))
+            target_params[m.group(1)] = m.group(2)
             res_list.append(resource)
     # Create dict of required policies with action: list(resources) pairs
     req_permissions = dict()
@@ -238,7 +238,8 @@ def _match_permissions(req_permissions: dict = None):
     return allow_match
 
 
-def list_handler(result, original: dict = None, allowed: dict = None, target: list = None, add_denied: bool = False,
+def list_handler(result: AffectedItemsWazuhResult, original: dict = None, allowed: dict = None, target: dict = None,
+                 add_denied: bool = False,
                  **post_proc_kwargs):
     """ Post processor for framework list responses with affected items and optional denied items
 
@@ -250,18 +251,17 @@ def list_handler(result, original: dict = None, allowed: dict = None, target: li
     :return: WazuhResult
     """
     if add_denied:
-        if len(target) == 1:
-            original_kwargs = original[target[0]] if isinstance(original[target[0]], list) else [original[target[0]]]
-            for item in set(original_kwargs) - set(list(allowed[list(allowed.keys())[0]])):
-                result['failed_items'].append(create_exception_dic(item, WazuhError(4000)))
-        else:
-            original_kwargs = original[target[1]] if isinstance(original[target[1]], list) else list(
-                original[target[1]])
-            for item in set(original_kwargs) - set(list(allowed[list(allowed.keys())[1]])):
-                result['failed_items'].append(create_exception_dic('{}:{}'.format(original[target[0]], item),
-                                                                   WazuhError(4000)))
+        for res_id, target_param in target.items():
+            denied = set(original[target_param]) - allowed[res_id]
+            for denied_item in denied:
+                result.add_failed_item(id_=denied_item, error=WazuhError(4000,
+                                                                         extra_message=f'Resource type: {res_id}'))
+    else:
+        if 'exclude_codes' in post_proc_kwargs:
+            for ex_code in post_proc_kwargs['exclude_codes']:
+                result.remove_failed_items(ex_code)
 
-    return data_response_builder(result, original=original, add_denied=add_denied, **post_proc_kwargs)
+    return result
 
 
 def expose_resources(actions: list = None, resources: list = None, post_proc_func: callable = list_handler,
@@ -272,8 +272,6 @@ def expose_resources(actions: list = None, resources: list = None, post_proc_fun
     :param resources: List of resources exposed by the framework function
     :param post_proc_func: Name of the function to use in response post processing
     :param post_proc_kwargs: Extra parameters used in post processing
-    :param stack: Flag that indicates if we should eliminate the key RBAC or not depending on whether the function is
-    decorated by more than one decorator
     :return: Allow or deny framework function execution
     """
     if post_proc_kwargs is None:
@@ -286,13 +284,18 @@ def expose_resources(actions: list = None, resources: list = None, post_proc_fun
                 _get_required_permissions(actions=actions, resources=resources, **kwargs)
             allow = _match_permissions(req_permissions=req_permissions)
             original_kwargs = copy.deepcopy(kwargs)
-            for index, target in enumerate(target_params):
+
+            for res_id, target_param in target_params.items():
                 try:
-                    # We don't have any permissions over the required permissions
-                    if len(allow[list(allow.keys())[index]]) == 0:
+                    # We don't have any permissions over the required resources
+                    if len(allow[res_id]) == 0 and \
+                            original_kwargs.get(target_param, None) is not None and \
+                            len(original_kwargs[target_param]) != 0:
                         raise Exception
-                    if target != '*':  # No resourceless
-                        kwargs[target] = list(allow[list(allow.keys())[index]])
+                    if target_param != '*':  # No resourceless
+                        kwargs[target_param] = list(allow[res_id])
+                    elif len(allow[res_id]) == 0:
+                        raise Exception
                 except Exception:
                     raise WazuhError(4000)
             result = func(*args, **kwargs)
@@ -305,66 +308,3 @@ def expose_resources(actions: list = None, resources: list = None, post_proc_fun
         return wrapper
 
     return decorator
-
-
-def _merge_errors(failed_items, add_denied, **kwargs):
-    """ Merges common errors into one, where a list of identifiers of the affected resource appears
-
-    :param failed_items: Dictionary with the errors occurred
-    :return: Final error list
-    """
-    code_ids = dict()
-    error_count = 0
-    for index, failed_item in enumerate(failed_items):
-        if not ('exclude_codes' in kwargs and
-                failed_item['error']['code'] in kwargs['exclude_codes'] and
-                add_denied is False):
-            if failed_item['error']['code'] not in code_ids.keys():
-                code_ids[failed_item['error']['code']] = dict()
-                code_ids[failed_item['error']['code']]['ids'] = list()
-                code_ids[failed_item['error']['code']]['index'] = index
-            code_ids[failed_item['error']['code']]['ids'].append(failed_item['id'])
-            error_count += 1
-    final_errors_list = list()
-    code_ids = dict(sorted(code_ids.items()))
-    for key, error_code in code_ids.items():
-        final_errors_list.append(failed_items[error_code['index']])
-        try:
-            final_errors_list[-1]['id'] = sorted(error_code['ids'], key=int)
-        except ValueError:
-            final_errors_list[-1]['id'] = sorted(error_code['ids'])
-    return final_errors_list, error_count
-
-
-def data_response_builder(result, original: dict = None, add_denied: bool = False, **post_proc_kwargs):
-    """
-
-    :param result: List with affected_items, failed_items and str_priority
-    :param original: Original input call parameter values
-    :param add_denied: Flag to add denied permissions to answer
-    :return: WazuhResult
-    """
-    try:
-        affected = sorted(result['affected_items'], key=int)
-    except ValueError:
-        affected = sorted(result['affected_items'])
-    except TypeError:
-        affected = result['affected_items']
-    final_dict = {'data': {'affected_items': affected,
-                           'total_affected_items': result['total_affected_items'] if 'total_affected_items' in result
-                           else len(result['affected_items'])}}
-
-    if result['failed_items']:
-        failed_result = _merge_errors(result['failed_items'], add_denied, **post_proc_kwargs)
-        final_dict['data']['failed_items'] = failed_result[0]
-        final_dict['data']['total_failed_items'] = failed_result[1]
-        final_dict['message'] = result['str_priority'][2] if not result['affected_items'] else result['str_priority'][1]
-    else:
-        final_dict['message'] = result['str_priority'][2] if not result['affected_items'] else result['str_priority'][0]
-    if 'extra_fields' in post_proc_kwargs:
-        for item in post_proc_kwargs['extra_fields']:
-            final_dict['data'][item] = original[item]
-    if 'extra_affected' in post_proc_kwargs:
-        final_dict['data'][post_proc_kwargs['extra_affected']] = result[post_proc_kwargs['extra_affected']]
-
-    return WazuhResult(final_dict, str_priority=result['str_priority'])
